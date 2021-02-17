fc424a1be4e7593b294be2b624543c8bfbf756ca
MandaTerence <randrianarideraterence@yahoo.fr>
2021-02-17 11:11:35 +0100

    First commit

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Homepage  acceuil
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    data() {
+        return {
+            //
+        }
+    },
+    created() {
+    },
+    methods: {}
+}
+</script>
